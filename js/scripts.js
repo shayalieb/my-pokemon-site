31982248a8b2ea951dd02dc3b4ecf852ea3a2cdd
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-07-14 12:20:02 -0400

    Updated the file to contain the projects data

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//This object variable will contain the Pokemon "name", "height", and type
+let pokemonList = [
+  {name: 'Metapod', type: 'Bug', height: 0.7}, 
+  {name: 'Weedle', type: ['bug', 'Poison'], height: 0.3},
+  {name: 'Fearow', type: ['flying', 'normal'], height: 1.2},
+  {name: 'Pikachu', type: 'Electric', height: 0.4}
+];
+
+
+
